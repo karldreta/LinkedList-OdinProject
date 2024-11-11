1ffa3f3fdfbbb630c5b1fb25aff5126c85c9aca2
karlDreta ツ <karldreta@gmail.com>
2024-11-12 01:51:31 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Template-Repo-01
+
+Welcome to the Template-Repo-01! This repository serves as a starting point for future projects with Webpack, providing a streamlined setup for development.
+
+### Getting Started
+
+Just type in `npm install` to your project and it should install all the dependencies of our project listed in **package.json**.
+
+Note: That this Template uses ES6 standards instead of the default CommonJS, verify the `type` in `package.json` -- it should be `  "type": "module"`.
+
+Moreover, **Webpack** should also be in ES6 format and should be using import/export -- check out `webpack.config`.
+
+If this doesn't work, check out **TEMPLATE.md** in this file and to know more about deployment.
+
+- Afterwards you may rewrite this file to align with your project, have fun!
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+import path from "path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+export default {
+  mode: "development",
+  entry: "./src/script.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve("dist"),
+    clean: true,
+  },
+  devtool: "eval-source-map",
+  devServer: {
+    watchFiles: ["./src/template.html"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+      // Will be injected to the head with defer by default.
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
