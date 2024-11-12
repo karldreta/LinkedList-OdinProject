bf0a887c2930d7c9cd20e2863f814021eb61a071
Karl James <karldreta@gmail.com>
2024-11-13 03:22:31 +0800

    Refactor head method

--- src/modules/linkedlist.js
+++ src/modules/linkedlist.js
@@ -2,21 +2,21 @@ import node from "./nodeModule.js";
 
 export default class LinkedList {
   constructor() {
-    this.head = null;
-    this.tail = null;
+    this.start = null;
+    this.end = null;
     this.length = 0;
   }
   append(value) {
     const newNode = new node();
     newNode.value = value; // We append the value after instantiation since the node class has default property values of null (which is okayt in this case).
-    if (this.head == null) {
-        this.head = newNode; // Link the current tail to the new node
-        this.tail = this.head; // Both head and tail should be equal for the first node
+    if (this.start == null) {
+        this.start = newNode; // Link the current end to the new node
+        this.end = this.start; // Both start and end should be equal for the first node
     }
     
-    if (this.tail != null) {
-        this.tail.next = newNode; // Link the current tail to be the newly added node.
-        this.tail = newNode; // Set the tail to the new node.
+    if (this.end != null) {
+        this.end.next = newNode; // Link the current end to be the newly added node.
+        this.end = newNode; // Set the end to the new node.
         this.length++; // Adds 1 to the size. *We only need to increment from here since this is where we set the node.
     }
   }
@@ -24,20 +24,24 @@ export default class LinkedList {
     const newNode = new node();
     newNode.value = value;
     
-    if (this.head == null) {
-        this.head = newNode; 
-        this.tail = this.head;
+    if (this.start == null) {
+        this.start = newNode; 
+        this.end = this.start;
     }
 
-    if (this.head != null) {
-        newNode.next = this.head; // Set the next value of the new node to the current head.
-        this.head = newNode; // Set the head to the newly created nede.
+    if (this.start != null) {
+        newNode.next = this.start; // Set the next value of the new node to the current start.
+        this.start = newNode; // Set the start to the newly created nede.
         this.length++; // Adds 1 to the size.
     }
 }
     size() {
-    return this.length; // Returns the total number of nodes in the list.
+        return this.length; // Returns the total number of nodes in the list.
 }
+    head() {
+        // We have to rename our properties to satisfy the project.
+        return this.start;
+    }
 
 }
 const list = new LinkedList();
@@ -49,17 +53,16 @@ list.append("Alpha");
 list.prepend("Flash");
 list.prepend("Josh");
 console.log(list.size());
+console.log(list.head());
 
 
 
 
-//   size() {
-//     return this.list.length; // Returns the total number of nodes in the list.
-//   }
-//   head() {
+
+//   start() {
 //     return this.list[0]; // Returns the first node in the list.
 //   }
-//   tail() {
+//   end() {
 //     return this.list[this.list.length - 1]; // Returns the last node in the list.
 //   }
 //   at(index) {
@@ -76,8 +79,8 @@ console.log(list.size());
 // list.prepend("Lina");
 // list.prepend("Marcy");
 // console.log(list.size());
-// console.log(list.head());
-// console.log(list.tail());
+// console.log(list.start());
+// console.log(list.end());
 // console.log(list.at(3));
 // console.log(list.pop());
 
